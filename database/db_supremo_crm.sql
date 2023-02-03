0c14ec2ebf91d05fe0d74ca40621f1188dc3ced2
allecosta <alle_costa10@outlook.com>
2023-02-03 14:41:49 -0300

    Add version infomation

--- database/db_supremo_crm.sql
+++ database/db_supremo_crm.sql
@@ -1,3 +1,10 @@
+-- MySQL Workbench SQL Dump
+-- version 8
+--
+-- Host: 127.0.0.1
+-- PHP Version: 8.1.3
+
+
 CREATE DATABASE db_supremo_crm;
 
 CREATE TABLE contacts (
@@ -6,4 +13,6 @@ CREATE TABLE contacts (
     phone VARCHAR(20) NOT NULL,
     cpf VARCHAR(14) NOT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
 
